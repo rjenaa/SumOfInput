1efb30a4c507807ec6d01efc2afba3b3a2a52425
Ray <54691205+rjenaa@users.noreply.github.com>
2020-09-11 09:28:42 -0400

    Did the sumofinput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    public static sum(int input){
+        int sum = 0;
+        for (int i = input; i > 0; i--){
+            sum+= i;
+        }
+        return sum;
+    }
+
 
     public static void main(String[] args){
+        System.out.println(sum(3));
 
     }
 }
